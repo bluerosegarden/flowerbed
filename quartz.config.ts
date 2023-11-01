bc597fb324a31fe9cf14dcdf4e2c8e187c918d79
Blue Rose <mail@bluerose.garden>
2023-11-01 13:02:55 -0500

    font change

--- quartz.config.ts
+++ quartz.config.ts
@@ -14,7 +14,7 @@ const config: QuartzConfig = {
     defaultDateType: "created",
     theme: {
       typography: {
-        header: "Schibsted Grotesk",
+        header: "Comfortaa",
         body: "Source Sans Pro",
         code: "IBM Plex Mono",
       },
